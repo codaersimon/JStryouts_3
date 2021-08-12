01397b01e9323e896d9f7ef375f225438c9780a6
codaersimon <codaersimon@gmail.com>
2021-08-12 17:58:17 +0200

    try

--- assets/js/Lev2_1.js
+++ assets/js/Lev2_1.js
@@ -1,11 +1,8 @@
 console.log("----Lev2_1----");
 
 let home = document.getElementById("navHome");
-console.log(home);
 
 function changeMe(){
     home.style.backgroundColor = "pink"
     home.style.transform = "matrix(-1, 0, 0, 1, 0, 0)"
 }
-
-console.log("----Lev2_1----");
--- assets/js/Lev2_2.js
+++ assets/js/Lev2_2.js
@@ -1,4 +1,4 @@
-console.log("test");
+console.log("----Lev2_2----");
 
 preName = document.getElementById("vorname");
 lastName = document.getElementById("nachname");
