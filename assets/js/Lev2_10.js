7cc027095f1e16707751715904ac211da02ca440
codaersimon <codaersimon@gmail.com>
2021-08-12 18:43:24 +0200

    final

--- assets/js/Lev2_10.js
+++ assets/js/Lev2_10.js
@@ -1,1 +1,22 @@
-document.getElement
+console.log("----Lev2_10----");
+
+let result = document.getElementById("title");
+
+function buttonSmall(){
+    result.classList.add("small")
+    result.classList.remove("middle")
+    result.classList.remove("crazy")
+    
+}
+function buttonMiddle(){
+    result.classList.add("middle")
+    result.classList.remove("small")
+    result.classList.remove("crazy")
+    
+}
+function buttonCrazy(){
+    result.classList.add("crazy")
+    result.classList.remove("middle")
+    result.classList.remove("small")
+    
+}
